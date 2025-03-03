e91d343a0a5153fc70fc869e35d643f691d91aef
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-03-04 04:03:45 +1100

    Added categories.

--- Api/MutualFundsApi.php
+++ Api/MutualFundsApi.php
@@ -78,6 +78,9 @@ class MutualFundsApi
         'getFundCategories' => [
             'application/json',
         ],
+        'getFundSchemeCategories' => [
+            'application/json',
+        ],
         'getFundSchemeDetails' => [
             'application/json',
         ],
@@ -645,6 +648,294 @@ class MutualFundsApi
 
 
         $resourcePath = '/mf/api/v4/fund_categories.json';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+
+
+
+
+        $headers = $this->headerSelector->selectHeaders(
+            ['application/json', ],
+            $contentType,
+            $multipart
+        );
+
+        // for model (json/xml)
+        if (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $formParamValueItems = is_array($formParamValue) ? $formParamValue : [$formParamValue];
+                    foreach ($formParamValueItems as $formParamValueItem) {
+                        $multipartContents[] = [
+                            'name' => $formParamName,
+                            'contents' => $formParamValueItem
+                        ];
+                    }
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif (stripos($headers['Content-Type'], 'application/json') !== false) {
+                # if Content-Type contains "application/json", json_encode the form parameters
+                $httpBody = \GuzzleHttp\Utils::jsonEncode($formParams);
+            } else {
+                // for HTTP post (form)
+                $httpBody = ObjectSerializer::buildQuery($formParams);
+            }
+        }
+
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $operationHost = $this->config->getHost();
+        $query = ObjectSerializer::buildQuery($queryParams);
+        return new Request(
+            'GET',
+            $operationHost . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+    /**
+     * Operation getFundSchemeCategories
+     *
+     * Get Fund Scheme Types and Categorories Returns
+     *
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['getFundSchemeCategories'] to see the possible values for this operation
+     *
+     * @throws \Apps\Fintech\Packages\Apis\Providers\Kuvera\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return array<string,string[]>
+     */
+    public function getFundSchemeCategories(string $contentType = self::contentTypes['getFundSchemeCategories'][0])
+    {
+        list($response) = $this->getFundSchemeCategoriesWithHttpInfo($contentType);
+        return $response;
+    }
+
+    /**
+     * Operation getFundSchemeCategoriesWithHttpInfo
+     *
+     * Get Fund Scheme Types and Categorories Returns
+     *
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['getFundSchemeCategories'] to see the possible values for this operation
+     *
+     * @throws \Apps\Fintech\Packages\Apis\Providers\Kuvera\ApiException on non-2xx response or if the response body is not in the expected format
+     * @throws \InvalidArgumentException
+     * @return array of array<string,string[]>, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function getFundSchemeCategoriesWithHttpInfo(string $contentType = self::contentTypes['getFundSchemeCategories'][0])
+    {
+        $request = $this->getFundSchemeCategoriesRequest($contentType);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? (string) $e->getResponse()->getBody() : null
+                );
+            } catch (ConnectException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    (int) $e->getCode(),
+                    null,
+                    null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+
+            switch($statusCode) {
+                case 200:
+                    if ('array<string,string[]>' === '\SplFileObject') {
+                        $content = $response->getBody(); //stream goes to serializer
+                    } else {
+                        $content = (string) $response->getBody();
+                        if ('array<string,string[]>' !== 'string') {
+                            try {
+                                $content = json_decode($content, false, 512, JSON_THROW_ON_ERROR);
+                            } catch (\JsonException $exception) {
+                                throw new ApiException(
+                                    sprintf(
+                                        'Error JSON decoding server response (%s)',
+                                        $request->getUri()
+                                    ),
+                                    $statusCode,
+                                    $response->getHeaders(),
+                                    $content
+                                );
+                            }
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, 'array<string,string[]>', []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+            }
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        (string) $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    (string) $response->getBody()
+                );
+            }
+
+            $returnType = 'array<string,string[]>';
+            if ($returnType === '\SplFileObject') {
+                $content = $response->getBody(); //stream goes to serializer
+            } else {
+                $content = (string) $response->getBody();
+                if ($returnType !== 'string') {
+                    try {
+                        $content = json_decode($content, false, 512, JSON_THROW_ON_ERROR);
+                    } catch (\JsonException $exception) {
+                        throw new ApiException(
+                            sprintf(
+                                'Error JSON decoding server response (%s)',
+                                $request->getUri()
+                            ),
+                            $statusCode,
+                            $response->getHeaders(),
+                            $content
+                        );
+                    }
+                }
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        'array<string,string[]>',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation getFundSchemeCategoriesAsync
+     *
+     * Get Fund Scheme Types and Categorories Returns
+     *
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['getFundSchemeCategories'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function getFundSchemeCategoriesAsync(string $contentType = self::contentTypes['getFundSchemeCategories'][0])
+    {
+        return $this->getFundSchemeCategoriesAsyncWithHttpInfo($contentType)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation getFundSchemeCategoriesAsyncWithHttpInfo
+     *
+     * Get Fund Scheme Types and Categorories Returns
+     *
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['getFundSchemeCategories'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function getFundSchemeCategoriesAsyncWithHttpInfo(string $contentType = self::contentTypes['getFundSchemeCategories'][0])
+    {
+        $returnType = 'array<string,string[]>';
+        $request = $this->getFundSchemeCategoriesRequest($contentType);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    if ($returnType === '\SplFileObject') {
+                        $content = $response->getBody(); //stream goes to serializer
+                    } else {
+                        $content = (string) $response->getBody();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        (string) $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'getFundSchemeCategories'
+     *
+     * @param  string $contentType The value for the Content-Type header. Check self::contentTypes['getFundSchemeCategories'] to see the possible values for this operation
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    public function getFundSchemeCategoriesRequest(string $contentType = self::contentTypes['getFundSchemeCategories'][0])
+    {
+
+
+        $resourcePath = '/mf/api/v4/fund_scheme_categories.json';
         $formParams = [];
         $queryParams = [];
         $headerParams = [];
